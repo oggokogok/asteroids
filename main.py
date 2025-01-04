bce3125735b2def498c3e5925734439a78da04ee
oggokogok <tsm9041@gmail.com>
2025-01-04 12:47:10 -0600

    able to move the triangle around

--- main.py
+++ main.py
@@ -15,12 +15,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
+        
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+        #print(dt)
+        
+        
         
              
 
