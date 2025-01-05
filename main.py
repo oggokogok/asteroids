fa939778e82a8c7336018527294c235924a83ffe
oggokogok <tsm9041@gmail.com>
2025-01-05 13:20:21 -0600

    updated to draw asteroids.

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    #initializing pygame, needed for following bits
     pygame.init()
+
+    #creating the display area
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Creating groups which will be iterated on directly
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+
+    #Adding items to the groups to be able to act upon them direction
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids,updatable,drawable)
     Player.containers = (updatable, drawable)
     clock = pygame.time.Clock()
     player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+    ast_field = AsteroidField()
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        #Call groups directly, not through player 
         for obj in updatable:
             obj.update(dt)
         
